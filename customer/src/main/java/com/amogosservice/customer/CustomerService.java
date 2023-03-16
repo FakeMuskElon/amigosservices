ff38b94517f963fadd26ec06d34179f69ebbc71a
koblevmurat@gmail.com <koblevmurat@gmail.com>
2023-03-17 00:00:40 +0300

    2

---
+++ customer/src/main/java/com/amogosservice/customer/CustomerService.java
@@ -0,0 +1,16 @@
+package com.amogosservice.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService() {
+    public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
+        Customer customer = Customer.builder().
+                firstName(customerRegistrationRequest.firstName())
+                .lastName(customerRegistrationRequest.lastName())
+                .email(customerRegistrationRequest.email())
+                .build();
+        //todo: check if email is valid and not taken
+        //todo: store customer in db
+    }
+}
